cd5809ef33ed94b946668b11e9fecfbdff56cb9e
sajid-pacho <sajid5552011@gmail.com>
2025-09-17 10:13:55 +0530

    First commit

---
+++ Evenorodd.java
@@ -0,0 +1,11 @@
+package Javaoperations;
+
+public class Evenorodd {
+
+	public static void main(String[] args) {
+		int Num = 86;
+	if(Num%2==0) {
+		System.out.println("even");
+	}else {System.out.println("odd");
+	}
+	}
